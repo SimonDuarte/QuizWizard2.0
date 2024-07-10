d3d1fbe76862f3cbd441a05e4b4de42cf0c8e200
SimonDuarte <simoda18@uia.no>
2024-07-10 13:09:22 +0200

    First commit. Added features

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex d:flex-row h-screen w-screen justify-center'>
+      Hello Mathilde
+    </div>
+  )
+}
+
+export default App
+
